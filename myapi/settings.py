2065b6431d39a0eeba6760e1ffdce040d9259421
Wandiyahyel Joseph <best9ja1@gmail.com>
2024-10-16 16:37:03 -0700

    push to render first updated

--- myapi/settings.py
+++ myapi/settings.py
@@ -80,11 +80,17 @@ WSGI_APPLICATION = 'myapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+     #   'ENGINE': 'django.db.backends.sqlite3',
+      #  'NAME': BASE_DIR / 'db.sqlite3',
+   # }
+#}
+import dj-database-url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
