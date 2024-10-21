5a4e4d010d366a47216fc86151fbcb7f3561eecc
Wandiyahyel Joseph <best9ja1@gmail.com>
2024-10-21 13:53:55 -0700

    push to render

--- myapi/settings.py
+++ myapi/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "static" 
-#STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles" 
+STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
